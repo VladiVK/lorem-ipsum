5275270676b05404bc3dcbf765411965d510b02c
VladiVK <kozobon@gmail.com>
2020-11-16 19:21:03 +0200

    initial form layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
 
 function App() {
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('cu-cu ' + count);
+  };
   return (
-    <div>
-      App.js
-    </div>
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type='submit' className="btn">genetate</button>
+      </form>
+    </section>
   );
 }
 
